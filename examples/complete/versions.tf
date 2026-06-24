2bc7474e2b75e218ad4543b689b4efaed6c9f9cf
venky <snikommuvenky115@gmail.com>
2026-06-24 22:06:09 +0100

    fix: pin provider versions to supported range

--- examples/complete/versions.tf
+++ examples/complete/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0"
+      version = ">= 5.0, < 7.0"
     }
     tls = {
       source  = "hashicorp/tls"
-      version = ">= 4.0.0"
+      version = ">= 4.0, < 6.0"
     }
   }
 }
